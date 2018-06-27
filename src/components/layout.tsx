019c81bea21c816fea44c3acd0eba4ebb8e93adc
Jose Diaz <jose.a.diaz@aguaseca.com>
2018-06-26 21:27:51 -0700

    add example using StaticQuery

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
+import { StaticQuery, graphql } from 'gatsby';
 
-const Layout: React.SFC = ({ children }) => (
-    <div>
-        <h1>Gatsby v2 Template with Typescript</h1>
-        {children}
-    </div>
-)
+interface LayoutProps {
+    children: ReactNode;
+}
+
+const Layout: React.SFC<LayoutProps> = ({ children }) => {
+    const siteName = "my Site name";
+    return (
+        <div>
+            <StaticQuery 
+                query={ graphql `query HeaderQuery {
+                    site {
+                        siteMetadata { title }
+                    }
+                }`}
+                render={ data => (
+                    <h1>{data.site.siteMetadata.title}</h1>
+                )}
+            />
+            
+            {children}
+        </div>
+    )
+}
 
 export default Layout;
