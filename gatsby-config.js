4b5e5bfb756809bf84c51846258649c515384b70
Jose Diaz <jose.a.diaz@aguaseca.com>
2018-06-26 19:35:35 -0700

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    siteMetadata: {
+      title: 'Gatsby Typescript Starter Template',
+    },
+    plugins: [`gatsby-plugin-typescript`],
+}
---
+++ src/components/layout.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Layout: React.SFC = ({ children }) => (
+    <div>
+        <h1>Gatsby v2 Template with Typescript</h1>
+        {children}
+    </div>
+)
+
+export default Layout;
