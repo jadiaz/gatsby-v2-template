a10b11a2a55774c02abcce0380d536bee028db25
Jose Diaz <jose.a.diaz@aguaseca.com>
2018-06-27 17:56:41 -0700

    add react-helmet to improve Lighthouse Accessibilty and SEO ranking
    - template specifies a valid language attribute
    - template now includes valid meta tags using Open Graph protocol
    - tempate specifies title tag

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,11 @@
 module.exports = {
     siteMetadata: {
+      name: 'Gatsby Template',
       title: 'Gatsby Typescript Starter Template',
+      description: 'Description of the site',
     },
-    plugins: [`gatsby-plugin-typescript`],
+    plugins: [
+      `gatsby-plugin-typescript`,
+      `gatsby-plugin-react-helmet`
+    ],
 }
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode } from 'react';
 import { StaticQuery, graphql } from 'gatsby';
-
+import { Helmet } from 'react-helmet';
 interface LayoutProps {
     children: ReactNode;
 }
@@ -11,12 +11,15 @@ const Layout: React.SFC<LayoutProps> = ({ children }) => {
         <div>
             <StaticQuery 
                 query={ graphql `query HeaderQuery {
-                    site {
-                        siteMetadata { title }
-                    }
+                    site { siteMetadata { name, title, description } }
                 }`}
-                render={ data => (
-                    <h1>{data.site.siteMetadata.title}</h1>
+                render={ (data) => (
+                    <Helmet defaultTitle={data.site.siteMetadata.title}>
+                        <html lang="en" />
+                        <meta property="og:type" content="website" />
+                        <meta property="og:site_name" content={data.site.siteMetadata.name} />
+                        <meta name="description" content={data.site.siteMetadata.description} />
+                    </Helmet>
                 )}
             />
             
