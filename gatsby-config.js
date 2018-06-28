9e48ba06a043e1bf1ff2d8af89a9293649a0da6e
Jose Diaz <jose.a.diaz@aguaseca.com>
2018-06-27 20:14:06 -0700

    add pwa and offline support.
    lighthouse:
    - performance   [98]
    - progressive     [91]
    - accessibility    [100]
    - best practices [94]
    - SEO                  [100]

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,19 @@ module.exports = {
     },
     plugins: [
       `gatsby-plugin-typescript`,
-      `gatsby-plugin-react-helmet`
+      `gatsby-plugin-react-helmet`,
+      {
+        resolve: `gatsby-plugin-manifest`,
+        options: {
+          name: "Gatsby-Typescript-Template",
+          short_name: "GTS-Template",
+          start_url: "/",
+          background_color: null,
+          theme_color: null,
+          display: "minimal-ui",
+          icon: "src/images/monogram.svg",
+        },
+      },
+      `gatsby-plugin-offline`,
     ],
 }
